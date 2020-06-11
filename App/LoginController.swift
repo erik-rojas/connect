2b5e8bd90d0cc3b3d7d6b71a0fdbceca65579730
Abraham Choe <abraham.choe@gmail.com>
2020-06-11 14:56:45 -0700

    fix: UI login test
    Co-authored-by: Paul Zabelin <paulz@users.noreply.github.com>

--- App/LoginController.swift
+++ App/LoginController.swift
@@ -8,11 +8,17 @@ public protocol AlertProtocol {
     func show(alert: NSAlert)
 }
 
+class NetworkService: NetworkServiceProtocol {
+    func login(username: String) -> String {
+        "success"
+    }
+}
+
 public class LoginController: NSViewController {
     @IBOutlet weak var environmentButton: NSPopUpButton!
     @IBOutlet weak var usernameField: NSTextField!
     @IBOutlet weak var passwordField: NSSecureTextField!
-    public var networkService: NetworkServiceProtocol!
+    public var networkService: NetworkServiceProtocol = NetworkService()
     public var alertService: AlertProtocol!
     @IBAction public func connectAction(_ sender: NSButton) {
         if networkService.login(username: "sparta@example.com") == "success" {
@@ -21,4 +27,5 @@ public class LoginController: NSViewController {
             alertService.show(alert: NSAlert())
         }
     }
+    
 }
